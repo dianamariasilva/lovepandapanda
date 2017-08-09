1f87184e9c9acee7997f322659c2d460f202eaf1
Diana Maria Silva Valencia <diana.silva.v94@gmail.com>
2017-08-09 14:04:19 -0500

    Pagina web panda terminada

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+var pictures = ["assets/img/a1.jpg","assets/img/a2.jpg", "assets/img/a3.jpg", "assets/img/a4.jpg"];
+pictures..setAttribute('class', 'inicio');
+
+var img_1=pictures[0];
+img_1.setAttribute('id','img1');}
+
+var img_2=pictures[1];
+img_2.setAttribute('id','img2');
+
+var img_3=pictures[2];
+img_3.setAttribute('id','img3');
+
+var img_4=pictures[3];
+img_4.setAttribute('id','img4');
+
